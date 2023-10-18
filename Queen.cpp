ccdda10fc5988b3616a5907a79c360247e056e9c
groupCMPR <144290496+groupCMPR@users.noreply.github.com>
2023-10-18 02:41:43 -0700

    Create Queen.cpp

---
+++ Queen.cpp
@@ -0,0 +1,179 @@
+#include "Queen.h"
+
+//default constructor
+Queen::Queen() {
+	size = 0;
+}
+
+//MUTATORS:-----------------------------------------------------------
+
+//Precondition: int newSize
+//Postcondition: set size of board (sizexsize)
+void Queen::set_Size(const int& newSize) {
+	size = newSize;
+}
+
+//Precondition: int column
+//Postcondition: set first line of stack board
+void Queen::set_Line(const int& column) {
+	for (int i = 0; i < size; ++i) {
+		if (i == (column))
+			board.push('Q');
+		else
+			board.push('_');
+	}
+
+}
+
+//Precondition: board is not empty (use set_Line), size is not 2 or 3
+//Postcondition: fill board_Numberxboard_Number with n queens solution 
+void Queen::set_Board() {
+
+	bool column_check = false; //end of column, used to check if column is empty
+
+	for (int iter = size; iter < (size * size); ++iter) {
+		//if end of column, will see if there is a Q later
+		if ((iter + 1) % size == 0) {
+			column_check = true;
+		}
+
+		//used to test queens in same territory, success is true if there are no queens there
+		int pos_check = iter - size;
+		bool success = true, dia_left_bound = true, dia_right_bound = true;
+		
+		board.push('_'); //placeholder to allow boundary check 
+
+		//territory check
+		for (int i = 1; pos_check > -1; ++i) {
+
+			//vertical  (from current position)
+			if (board._Get_container().at(pos_check) == 'Q') {
+				success = false;
+				break;
+			}
+
+			//diagonal right (from current position), stops when it is at most right direction
+			if (dia_right_bound) {
+				if ((pos_check + i) % size == 0 || (iter + 1) % size == 0)
+					dia_right_bound = false;
+				else if (board._Get_container().at(pos_check + i) == 'Q') {
+					success = false;
+					break;
+				}
+			}
+
+			//diagonal left (from current position), , stops when it is at most left direction
+			if (dia_left_bound) {
+				if ((pos_check - i) % size == (size - 1) || iter % size == 0 || (pos_check - i) < 0)
+					dia_left_bound = false;
+				else if (board._Get_container().at(pos_check - i) == 'Q') {
+					success = false;
+					break;
+				}
+			}
+			pos_check -= size;
+		}
+
+		if (!success && column_check) {
+
+			//pops until the q
+			while (true) {
+				board.pop();
+				--iter;
+				if (board._Get_container().at(iter) == 'Q')
+					break;
+			}
+
+
+			//if popping stack at less than size, pops everything, and refills everything
+			//with a readjusted Q in the correct position
+			//else if last Q is at the end of the stack column, erases everything except previous Q, then pops that Q and pushes _ 
+			//else pops the placeholder and pushes Q
+			if (iter + 1 == size) {
+				int position = 0;
+
+				for (int i = size - 1; i > -1; --i) {
+					if (board._Get_container().at(i)) {
+						if ( (i + 1) % size == 0)
+							position = 0;
+						else
+							position = i + 1;
+					}
+					board.pop();
+				}
+				for (int i = 0; i < size; ++i) {
+					if (position == i) {
+						board.push('Q');
+					}
+					else
+						board.push('_');
+				
+				}
+			}
+			else if ((iter + 1) % size == 0) {
+				while (true) {
+					board.pop();
+					--iter;
+					if (board._Get_container().at(iter) == 'Q')
+						break;
+				}
+
+				board.pop();
+				board.push('_');
+			}
+			else {
+				board.pop();
+				board.push('_');
+			}
+
+			column_check = false; 
+
+		}
+		else if (success) {
+			board.pop();
+			board.push('Q');
+
+			//pushes in _ for remainder of line
+			for (int i = iter; iter < i + (size - (i % size)) - 1; ++iter)
+				board.push('_');
+
+			column_check = false;
+		}
+	}
+}
+
+
+//---------------------------------------------------------------------
+//							ACCESSORS
+//---------------------------------------------------------------------
+
+//Precondition: class Queen, outstream
+//Postcondition: displays all elements in stack of Queen
+ostream& operator<< (ostream& out, const Queen& obj) {
+	out << "\n\t" << obj.size << "-Queens";
+	int i = 1;
+	//displays menu
+	//top of board:
+	out << "\n\t" << char(201) << string((obj.size * 2 - 1), char(205)) << char(187) << "\n\t";
+
+	//middle of board: char and side |
+	for (auto iter = obj.board._Get_container().begin(); iter != obj.board._Get_container().end(); ++iter, ++i) {
+
+		if (i == 1 || i % obj.size == 1)
+			cout << char(186);
+
+		out << *iter;
+
+		if (i % obj.size == 0)
+			out << char(186) << "\n\t";
+		else
+			out << char(179);
+
+	}
+
+	//end of board:
+	out << char(200) << string((obj.size * 2 - 1), char(205)) << char(188);
+
+	return out;
+}
+
